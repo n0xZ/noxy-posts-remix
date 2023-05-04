6c45af7fbf8b23fd3341032068a2324e1da53eeb
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-03 21:18:51 -0300

    setup post-list

---
+++ app/components/post/list.tsx
@@ -0,0 +1,15 @@
+import type { Post } from '@prisma/client'
+import PostItem from './item'
+
+type Props = {
+	posts: Post[]
+}
+export function PostList(props: Props) {
+	return (
+		<section className="container grid h-full max-w-3xl grid-rows-2 p-3 mx-auto mt-6 xl:p-0 ">
+			{props.posts.map((p) => (
+				<PostItem post={p} key={p.id} />
+			))}
+		</section>
+	)
+}
