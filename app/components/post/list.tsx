dafd0083a69130393f351864ebccb028db8dec05
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-04 16:15:33 -0300

    chore: update marginT value

--- app/components/post/list.tsx
+++ app/components/post/list.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 export function PostList(props: Props) {
 	return (
-		<section className="container grid h-full max-w-3xl grid-rows-2 p-3 mx-auto mt-6 xl:p-0 ">
+		<section className="container grid h-full max-w-3xl grid-rows-2 p-3 mx-auto mt-8 xl:p-0 ">
 			{props.posts.map((p) => (
 				<PostItem post={p} key={p.id} />
 			))}
