46a434141f50780eb40a12e27901f8d90887e790
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-04 14:27:02 -0300

    chore: add error boundary on homepage

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,38 +1,52 @@
 import type { Post } from '@prisma/client'
 import type { LoaderArgs, V2_MetaFunction } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
+import { Link, useLoaderData, useRouteError } from '@remix-run/react'
+import { HomeLayout } from '~/components/layout/home'
 import { PostList } from '~/components/post/list'
 import { getPostsByUserId } from '~/models/post.server'
-import { prisma } from '~/utils/prisma.server'
 import { requireUserId } from '~/utils/session.server'
 
 type LoaderResponse = {
 	posts: Post[]
-	avatarUrl: string
 } | null
 export const loader = async ({ request }: LoaderArgs) => {
 	const id = await requireUserId(request)
 	const existingPosts = await getPostsByUserId(id)
-	const user = await prisma.user.findUnique({ where: { id } })
 
 	return {
 		posts: existingPosts,
-		avatarUrl: user?.avatarUrl,
 	}
 }
 
 export const meta: V2_MetaFunction = () => [{ title: 'Noxy - posts | Home' }]
+
+export const ErrorBoundary = () => {
+	const error = useRouteError()
+	if (error instanceof Error)
+		return (
+			<HomeLayout>
+				<h2 className="text-2xl text-center xl:text-4xl">
+					Al parecer ocurrió un error.
+				</h2>
+				<p>Vuelva a intentarlo más tarde</p>
+			</HomeLayout>
+		)
+}
 export default function Home() {
 	const loaderData = useLoaderData() as unknown as LoaderResponse
 
 	return (
-		<main className="container w-full max-w-3xl min-h-screen mx-auto mt-12">
-			<Link to="/home/posts/create">Crear proyecto</Link>
-			<h2 className="text-3xl font-bold text-center">Tus posts recientes</h2>
+		<HomeLayout>
+			<h2 className="text-3xl font-bold text-center ">Tus posts recientes</h2>
 
-			{loaderData && loaderData.posts.length ? (
+			{loaderData && loaderData.posts.length !== 0 ? (
 				<PostList posts={loaderData.posts} />
-			) : null}
-		</main>
+			) : (
+				<>
+					<h3>Al parecer no tiene ningún post creado hasta el momento.</h3>
+					<Link to="/home/post/create">Empezá a crear uno haciendo click acá</Link>
+				</>
+			)}
+		</HomeLayout>
 	)
 }
